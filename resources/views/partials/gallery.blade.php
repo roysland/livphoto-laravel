534bf12104fc235f07811955e565adb2d31bd713
Øyvind Røysland <roysland@gmail.com>
2023-09-30 20:58:23 +0200

    asfs

--- resources/views/partials/gallery.blade.php
+++ resources/views/partials/gallery.blade.php
@@ -1,6 +1,6 @@
 <section class="image-grid">
     @foreach($images as $image)
-    <figure data-id="{{$image->id}}" data-order="{{$image->gallery_order}}" data-folder={{$image->category}} data-src={{$image->url}} tab-index="0">
+    <figure data-id="{{$image->id}}" data-description="{{$image->description}}" data-order="{{$image->gallery_order}}" data-folder={{$image->category}} data-src={{$image->url}} tab-index="0">
         <picture>
             <source media="(min-width: 768px)" srcset={{ $image->url }} />
             <source media="(min-width: 0px)" srcset={{ $image->url }} />
