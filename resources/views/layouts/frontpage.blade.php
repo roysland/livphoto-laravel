3ed17621448533371185da014c7ed24e91107ab6
Øyvind Røysland <roysland@gmail.com>
2023-12-03 19:06:54 +0100

    fix front

--- resources/views/layouts/frontpage.blade.php
+++ resources/views/layouts/frontpage.blade.php
@@ -10,8 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <link rel="stylesheet" href="/bundle1.css">
-        @vite(['resources/css/bundle1.scss', 'resources/js/app.js'])
+        @vite(['resources/css/bundle.scss', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
         @yield('content')
